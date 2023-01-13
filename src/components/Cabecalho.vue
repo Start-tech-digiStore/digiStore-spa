9500b19ede4b593d7809323bf2f29853885706c1
Sérgio Augusto David Jr <sergio.adjr@gmail.com>
2023-01-13 12:35:14 -0400

    correçao cabecalho

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -1,19 +1,14 @@
 <template>
-    <div class="cabecalho">
-      <v-toolbar
-        dark
-        prominent
-        color="black"
+  <div class="cabecalho">
+    <v-toolbar dark prominent color="black">
+      <img src="../../public/logo-favicon2.png" style="max-width: 80px;">
+      <v-toolbar-title>DigiStore</v-toolbar-title>
 
-      >
-        <img src="../../public/logo-favicon2.png" style="max-width: 80px;">  
-        <v-toolbar-title>DigiStore</v-toolbar-title>
+      <v-spacer></v-spacer>
 
-        <v-spacer></v-spacer>
-      
-        <v-btn icon>
-          <v-icon>mdi-export</v-icon>
-        </v-btn>
-      </v-toolbar>
-    </div>
-  </template>
+      <v-btn icon>
+        <v-icon>mdi-export</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </div>
+</template>
